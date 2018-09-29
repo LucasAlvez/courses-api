e473a891ac30a6599fe8152e3610415535bc808d
Lucas Alves <luucasalves.p@gmail.com>
2018-09-29 20:35:55 -0300

    Roles refactor

--- src/main/java/com/courses/api/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.courses.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.courses.api.entity.RoleEntity;
-
-public interface RoleRepository extends JpaRepository<RoleEntity, Long>{
-
-}
