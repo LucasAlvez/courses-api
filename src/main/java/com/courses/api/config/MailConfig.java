c50daf8c1c9d22aa25efbefe0d92998468ca22d0
Lucas Alves <luucasalves.p@gmail.com>
2018-11-30 21:29:09 -0200

    mail

---
+++ src/main/java/com/courses/api/config/MailConfig.java
@@ -0,0 +1,33 @@
+package com.courses.api.config;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration 
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailService() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+
+        javaMailSender.setHost("smtp.gmail.com");
+        javaMailSender.setPort(465);
+
+        javaMailSender.setJavaMailProperties(getMailProperties());
+
+        return javaMailSender;
+    }
+
+    private Properties getMailProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("mail.transport.protocol", "smtp");
+        properties.setProperty("mail.smtp.auth", "false");
+        properties.setProperty("mail.smtp.starttls.enable", "false");
+        properties.setProperty("mail.debug", "false");
+        return properties;
+    }
+}
