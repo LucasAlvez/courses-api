2bd242dafc9533e1a4180c0ecc9f424190916032
Lucas Alves <luucasalves.p@gmail.com>
2018-11-30 21:13:29 -0200

    prod

--- src/main/java/com/courses/api/service/email/EmailService.java
+++ src/main/java/com/courses/api/service/email/EmailService.java
@@ -1,11 +1,9 @@
 package com.courses.api.service.email;
 
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.stereotype.Service;
 
 import com.courses.api.entity.UserEntity;
 
-@Service
 public interface EmailService {
 	
 	public void confirmationOfRegistration (UserEntity account);
--- src/main/java/com/courses/api/service/email/SmtpEmailService.java
+++ src/main/java/com/courses/api/service/email/SmtpEmailService.java
@@ -3,7 +3,9 @@ package com.courses.api.service.email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SmtpEmailService extends AbstractEmailService {
 	
 	@Autowired
