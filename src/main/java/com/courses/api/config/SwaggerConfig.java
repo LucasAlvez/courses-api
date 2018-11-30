e04eca521d75cfb8b1f800f2ddafff2db9f8ca8b
Lucas Alves <luucasalves.p@gmail.com>
2018-11-30 21:00:59 -0200

    prod

--- src/main/java/com/courses/api/config/SwaggerConfig.java
+++ src/main/java/com/courses/api/config/SwaggerConfig.java
@@ -4,7 +4,6 @@ import static springfox.documentation.builders.PathSelectors.regex;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -18,17 +17,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-	
 
 	@Bean
-	 public Docket productApi() {
-	        return new Docket(DocumentationType.SWAGGER_2)
-	                .select()
-	                .apis(RequestHandlerSelectors.basePackage("com.courses.api"))
-	                .paths(regex("/v1.*"))
-	                .build()
-	                .apiInfo(metaInfo());
-	    }
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.courses.api")).paths(regex("/v1.*")).build()
+				.apiInfo(metaInfo());
+	}
 
 	private ApiInfo metaInfo() {
 		ApiInfo apiInfo = new ApiInfo("Cursos API REST", "", "1.0", "Terms of Service",
@@ -36,10 +31,9 @@ public class SwaggerConfig {
 				"https://www.apache.org/licesen.html");
 		return apiInfo;
 	}
-	
+
 	@Bean
 	public SecurityConfiguration security() {
-		return new SecurityConfiguration(null, null, null, null, "token", ApiKeyVehicle.HEADER,
-				"Authorization", ",");
+		return new SecurityConfiguration(null, null, null, null, "token", ApiKeyVehicle.HEADER, "Authorization", ",");
 	}
 }
