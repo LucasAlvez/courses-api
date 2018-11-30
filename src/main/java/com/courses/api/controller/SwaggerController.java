f295605fbcfbd4c6ebf203d147ea566ccbec1e9f
Lucas Alves <luucasalves.p@gmail.com>
2018-11-30 20:57:42 -0200

    prod

--- src/main/java/com/courses/api/controller/SwaggerController.java
+++ src/main/java/com/courses/api/controller/SwaggerController.java
@@ -1,11 +1,9 @@
 package com.courses.api.controller;
 
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@Profile("dev")
 public class SwaggerController {
 	
 	@RequestMapping("/")
