8fb69dd21698e3c9570aa50984d6a051d723460d
Lucas Alves <luucasalves.p@gmail.com>
2018-11-30 21:21:02 -0200

    prod

--- src/main/java/com/courses/api/service/email/SmtpEmailService.java
+++ src/main/java/com/courses/api/service/email/SmtpEmailService.java
@@ -1,15 +1,15 @@
 package com.courses.api.service.email;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SmtpEmailService extends AbstractEmailService {
 	
 	@Autowired
-	private MailSender mailSender;
+	private JavaMailSender mailSender;
 
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
