f48a18aeba2e3ce05f7dc17b197a793be3b21015
Lucas Alves <luucasalves.p@gmail.com>
2018-11-30 22:19:43 -0200

    build

--- src/main/java/com/courses/api/service/email/SmtpEmailService.java
+++ src/main/java/com/courses/api/service/email/SmtpEmailService.java
@@ -1,7 +1,6 @@
 package com.courses.api.service.email;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
