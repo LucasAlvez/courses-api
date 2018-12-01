73ada508fa9e0e964d9b7b46b1f8b97d1bf451dc
Lucas Alves <luucasalves.p@gmail.com>
2018-11-30 22:06:29 -0200

    email

--- src/main/java/com/courses/api/service/email/EmailService.java
+++ src/main/java/com/courses/api/service/email/EmailService.java
@@ -1,9 +1,11 @@
 package com.courses.api.service.email;
 
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
 
 import com.courses.api.entity.UserEntity;
 
+@Service
 public interface EmailService {
 	
 	public void confirmationOfRegistration (UserEntity account);
