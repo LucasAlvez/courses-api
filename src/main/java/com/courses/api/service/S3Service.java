803b60845458960fb1916125b5c7271faeb4b607
Lucas Alves <luucasalves.p@gmail.com>
2018-12-04 21:46:05 -0200

    s3 upload

---
+++ src/main/java/com/courses/api/service/S3Service.java
@@ -0,0 +1,46 @@
+package com.courses.api.service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+
+@Service
+public class S3Service {
+
+	@Autowired
+	private AmazonS3 s3client;
+
+	@Value("${s3.bucket}")
+	private String bucketName;
+
+	public URI uploadFile(MultipartFile multipartFile) throws Exception {
+		try {
+			String fileName = multipartFile.getOriginalFilename();
+			InputStream is = multipartFile.getInputStream();
+			String contentType = multipartFile.getContentType();
+			return uploadFile(is, fileName, contentType);
+		} catch (IOException e) {
+			throw new Exception("Erro de IO: " + e.getMessage());
+		}
+	}
+
+	public URI uploadFile(InputStream is, String fileName, String contentType) throws Exception {
+		try {
+			ObjectMetadata meta = new ObjectMetadata();
+			meta.setContentType(contentType);
+			s3client.putObject(bucketName, fileName, is, meta);
+			return s3client.getUrl(bucketName, fileName).toURI();
+		} catch (URISyntaxException e) {
+			throw new Exception("Erro ao converter URL para URI");
+		}
+	}
+}
