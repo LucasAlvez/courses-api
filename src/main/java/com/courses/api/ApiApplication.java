5d040299a88966d0bc937a1c25b862db01b7164f
Lucas Alves <luucasalves.p@gmail.com>
2019-01-06 16:11:25 -0200

    RDS AWS

--- src/main/java/com/courses/api/ApiApplication.java
+++ src/main/java/com/courses/api/ApiApplication.java
@@ -2,9 +2,16 @@ package com.courses.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApiApplication {
+public class ApiApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApiApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
